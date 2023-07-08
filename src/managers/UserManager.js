c98ab91c61b8655c9c98de33d388671a6dcbceea
elijah-wright <elijah@elijahs.space>
2023-07-08 09:17:13 -0700

    add follow endpoint; fix tabs/semicolons

--- src/managers/UserManager.js
+++ src/managers/UserManager.js
@@ -2,7 +2,13 @@ const RESTManager = require('./RESTManager');
 
 class UserManager extends RESTManager {
 	async fetch(user) {
-		return await this.request(`/api/v1/users/${user}/info`)
+		return await this.request(`/api/v1/users/${user}/info`);
+	}
+
+	async follow(user) {
+		return await this.request(`/api/v1/friendships/create/${user}/`, {
+			method: 'POST',
+		});
 	}
 }
 
