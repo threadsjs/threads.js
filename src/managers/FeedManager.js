ebfa329b2c33305e38ff414c5ab832302aaf9ccf
elijah-wright <elijah@elijahs.space>
2023-07-08 11:46:26 -0700

    split fetch in FeedManager

--- src/managers/FeedManager.js
+++ src/managers/FeedManager.js
@@ -1,8 +1,12 @@
 const RESTManager = require('./RESTManager');
 
 class FeedManager extends RESTManager {
-	async fetch(user) {
+	async fetchThreads(user) {
 		return await this.request(`/api/v1/text_feed/${user}/profile`);
+	}
+
+	async fetchReplies(user) {
+		return await this.request(`/api/v1/text_feed/${user}/profile/replies`);
 	}
 
 	async recommended() {
