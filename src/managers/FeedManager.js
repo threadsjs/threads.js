222523249ae8c43bf6793123063cc08a3451541c
elijah-wright <elijah@elijahs.space>
2023-07-08 11:28:25 -0700

    add recommended users

--- src/managers/FeedManager.js
+++ src/managers/FeedManager.js
@@ -4,6 +4,10 @@ class FeedManager extends RESTManager {
 	async fetch(user) {
 		return await this.request(`/api/v1/text_feed/${user}/profile`);
 	}
+
+	async recommended() {
+		return await this.request('/api/v1/text_feed/recommended_users/');
+	}
 }
 
 module.exports = FeedManager;
