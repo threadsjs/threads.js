97331acfd7fef68ee65c97925cd6d35c6985573d
Abu Masyail <20874779+sooluh@users.noreply.github.com>
2023-07-09 23:54:13 +0700

    2fa support

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-const EventEmitter = require('node:events');
 const { fetch } = require("undici");
-const RESTManager = require('./managers/RESTManager');
-const UserManager = require('./managers/UserManager');
-const PostManager = require('./managers/PostManager');
-const FeedManager = require('./managers/FeedManager')
+const EventEmitter = require("node:events");
+const { parseBloksResponse } = require("./utils");
+const RESTManager = require("./managers/RESTManager");
+const UserManager = require("./managers/UserManager");
+const PostManager = require("./managers/PostManager");
+const FeedManager = require("./managers/FeedManager");
+
+const androidId = (Math.random() * 1e24).toString(36);
 
 async function getToken(username, password) {
-	const id = (Math.random() * 1e24).toString(36);
-	const url =
-		"https://i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/";
+	const base = "https://i.instagram.com";
+	const loginUrl = "/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/";
 
 	const params = {
 		client_input_params: {
 			password: password,
 			contact_point: username,
-			device_id: `android-${id}`,
+			device_id: `android-${androidId}`,
 		},
 		server_params: {
 			credential_type: "password",
-			device_id: `android-${id}`,
+			device_id: `android-${androidId}`,
 		},
 	};
 
@@ -43,24 +45,76 @@ async function getToken(username, password) {
 			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
 		},
 		body: `params=${encodeURIComponent(
-			requestBody.params
+			requestBody.params,
 		)}&bk_client_context=${encodeURIComponent(
-			requestBody.bk_client_context
+			requestBody.bk_client_context,
 		)}&bloks_versioning_id=${requestBody.bloks_versioning_id}`,
 	};
 
-	const response = await fetch(url, requestOptions);
+	const response = await fetch(base + loginUrl, requestOptions);
 	const text = await response.text();
-	const pos = text.search("Bearer IGT:2:");
-	const sliced = text.substring(pos);
-	const token = sliced.substring(13, sliced.search(/\\\*?/gm));
-	return token;
+	const bloks = parseBloksResponse(text);
+
+	if (bloks.two_factor_required) {
+		const {
+			two_factor_identifier,
+			trusted_notification_polling_nonce
+		} = bloks.two_factor_info
+
+		console.log('Please approve the login request on your Instagram')
+
+		const token = await new Promise((resolve) => {
+			const statusUrl = "/api/v1/two_factor/check_trusted_notification_status/";
+			const verifyUrl = "/api/v1/accounts/two_factor_login/";
+
+			const interval = setInterval(async function () {
+				requestOptions.body = new URLSearchParams({
+					two_factor_identifier: two_factor_identifier,
+					username,
+					device_id: `android-${androidId}`,
+					trusted_notification_polling_nonces:
+						JSON.stringify([trusted_notification_polling_nonce]),
+				}).toString();
+
+				const response = await fetch(base + statusUrl, requestOptions);
+				const json = await response.json();
+
+				if (json.review_status === 1) {
+					requestOptions.body = new URLSearchParams({
+						signed_body: 'SIGNATURE.' + JSON.stringify({
+							verification_code: '',
+							two_factor_identifier,
+							username,
+							device_id: `android-${androidId}`,
+							trusted_notification_polling_nonces:
+								JSON.stringify([trusted_notification_polling_nonce]),
+							verification_method: '4'
+						})
+					}).toString()
+
+					const response = await fetch(base + verifyUrl, requestOptions);
+					const header = response.headers.get('Ig-Set-Authorization');
+
+					clearInterval(interval);
+					const token = header.replace("Bearer IGT:2:", "") || null
+
+					resolve(token);
+				}
+			}, 2_500);
+		});
+
+		return token;
+	}
+
+	return (
+		bloks.headers?.["IG-Set-Authorization"].replace("Bearer IGT:2:", "") || null
+	);
 }
 
 class Client extends EventEmitter {
 	constructor({ token, userAgent, appId }) {
 		super({ captureRejections: true });
-		
+
 		if (!token) {
 			throw new Error("Token is required.");
 		}
@@ -68,7 +122,7 @@ class Client extends EventEmitter {
 		this.token = token;
 		this.userAgent = userAgent || "Barcelona 289.0.0.77.109 Android";
 		this.appId = appId || "238260118697367";
-		this.androidId = (Math.random() * 1e24).toString(36);
+		this.androidId = androidId;
 
 		this.rest = new RESTManager(this);
 
