e49bac5acea8eed3139a56f4124988a927460778
elijah-wright <elijah@elijahs.space>
2023-07-10 07:39:11 -0700

    potentially null token

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class Client extends EventEmitter {
 	constructor({ token, userAgent, appId }) {
 		super({ captureRejections: true });
 
-		this.token = token;
+		this.token = token || null;
 		this.userAgent = userAgent || "Barcelona 289.0.0.77.109 Android";
 		this.appId = appId || "238260118697367";
 		this.androidId = androidId;
