90eeabf888639221bcee95f78ab748c654e8710a
Elijah Wright <elijah@elijahs.space>
2024-04-13 16:27:54 -0700

    fix post()

--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -2,17 +2,33 @@ const RESTManager = require('./RESTManager');
 
 class PostManager extends RESTManager {
 	async post(options) {
+		let container;
         if (typeof(options) === 'string') {
-            return await this.request(`/v19.0/me/threads?media_type=IMAGE&text=${options}`, {
+            await this.request(`/v19.0/me/threads?media_type=IMAGE&text=${options}`, {
 				method: 'POST'
+			}).then(res => {
+				containerId = res.id;
 			});
         } else {
 			// TODO: not this
 			let mediaType = options.mediaType === 'video' ? 'video' : 'image'
-            return await this.request(`/v19.0/me/threads?media_type=${mediaType.toUpperCase()}&${mediaType}_url=${options.url}&is_carousel_item=${options.carouselItem ? 'true' : 'false'}`, {
-				method: 'POST'
-			});
+            if (options.carouselItem) {
+				await this.request(`/v19.0/me/threads?media_type=${mediaType.toUpperCase()}&${mediaType}_url=${options.url}`, {
+					method: 'POST'
+				}).then(res => {
+					container = res.id;
+				});
+			} else {
+				await this.request(`/v19.0/me/threads?media_type=CAROUSEL&children=${options.url}&is_carousel_item=true`, {
+					method: 'POST'
+				}).then(res => {
+					container = res.id;
+				});
+			}
         }
+		return await this.request(`/v19.0/me/threads_publish?creation_id=${container}`, {
+			method: 'POST'
+		})
 	}
 }
 
