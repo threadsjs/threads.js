4ea0ce3a76f6f584c799bd90392cc47c75aa28da
Elijah Wright <git@elijahs.space>
2024-04-14 23:29:49 +0000

    update PostManager endpoints

--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -4,7 +4,7 @@ class PostManager extends RESTManager {
 	async post(options) {
 		let container;
         if (typeof(options) === 'string') {
-            await this.request(`/v19.0/me/threads?media_type=IMAGE&text=${options}`, {
+            await this.request(`/me/threads?media_type=IMAGE&text=${options}`, {
 				method: 'POST'
 			}).then(res => {
 				containerId = res.id;
@@ -13,20 +13,20 @@ class PostManager extends RESTManager {
 			// TODO: not this
 			let mediaType = options.mediaType === 'video' ? 'video' : 'image'
             if (options.carouselItem) {
-				await this.request(`/v19.0/me/threads?media_type=${mediaType.toUpperCase()}&${mediaType}_url=${options.url}`, {
+				await this.request(`/me/threads?media_type=${mediaType.toUpperCase()}&${mediaType}_url=${options.url}`, {
 					method: 'POST'
 				}).then(res => {
 					container = res.id;
 				});
 			} else {
-				await this.request(`/v19.0/me/threads?media_type=CAROUSEL&children=${options.url}&is_carousel_item=true`, {
+				await this.request(`/me/threads?media_type=CAROUSEL&children=${options.url}&is_carousel_item=true`, {
 					method: 'POST'
 				}).then(res => {
 					container = res.id;
 				});
 			}
         }
-		return await this.request(`/v19.0/me/threads_publish?creation_id=${container}`, {
+		return await this.request(`/me/threads_publish?creation_id=${container}`, {
 			method: 'POST'
 		})
 	}
